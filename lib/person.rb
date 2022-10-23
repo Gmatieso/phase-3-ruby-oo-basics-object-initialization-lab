0d3b0f04554a46c3eb75223fceae974a14b8c84e
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-24 02:17:12 +0300

    :wrench: created our initialized method

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+#Note Initialization method is similar method to the constructor method in javascript 
+class Person
+
+    #created an instance macro variable 
+
+    attr_reader: name
+
+    #created our initialization method passed variable name 
+    def initialize(name)
+        @name = name
+    end
+
+    
+end
