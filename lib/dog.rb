8b1c599f3fea927f970037d25816eeb2d469bd42
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-24 02:47:01 +0300

    :wrench: instance macro variable name and breed

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,9 @@
 #Note Initialization method is similar to the constructor method in JavaScript 
 class Dog
 
+    #instance macro variable name and breed 
+    attr_reader :name , :breed
+
 #Dog initialize with Name and Breed defaulting to "Mutt"
     def initialize(name,breed="Mutt")
         @name = name
